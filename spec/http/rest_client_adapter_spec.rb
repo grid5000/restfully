84b43116fc8f4cabfd56a28008455e7b52b04e66
Cyril Rohr <cyril.rohr@gmail.com>
2010-02-05 10:47:56 +0100

    fixed small bug related to new version of RestClient

--- spec/http/rest_client_adapter_spec.rb
+++ spec/http/rest_client_adapter_spec.rb
@@ -22,7 +22,7 @@ describe Restfully::HTTP::Adapters::RestClientAdapter do
     lambda{adapter.put(mock("restfully request"))}.should raise_error NotImplementedError, "PUT is not supported by your adapter."
   end
   it "should rescue any RestClient::Exception and correctly populate the response" do
-    res = mock(Net::HTTPResponse, :code => 404, :body => '{"message":"whatever"}', :to_hash => {'Content-Type' => 'application/json;charset=utf-8', 'Content-Length' => 22}, :[] => '')
+    res = mock(RestClient::Response, :code => 404, :to_s => '{"message":"whatever"}', :headers => {:content_type => 'application/json;charset=utf-8', :content_length => 22})
     RestClient::Resource.should_receive(:new).and_raise RestClient::ResourceNotFound.new(res)
     adapter = Restfully::HTTP::Adapters::RestClientAdapter.new("https://api.grid5000.fr", :username => 'crohr', :password => 'password')
     response = adapter.get(mock("request", :uri => "uri"))
